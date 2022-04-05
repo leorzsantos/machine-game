f76d07dd4080c51962e48c96d39b95101cf74d47
Leo Rezende <leorezende742@hotmail.com>
2022-04-05 20:54:14 -0300

    Begin a project

---
+++ src/components/CardGame/index.js
@@ -0,0 +1,11 @@
+import './style.css'
+
+function CardGame() {
+    return `
+    <article class="card-game">
+    <img src="images/alura-pixel.png" alt="Logo Alura pixel Art">
+    </article>
+    `
+}
+
+export default CardGame;
